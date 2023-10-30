6b73340108667fb449376d0a6145d84d84b8aad3
maxirosanda <maxi_rosanda@hotmail.com>
2023-10-30 18:37:10 -0300

    funciones promedio de nota y fecha de finalizacion curso

---
+++ funciones.sql
@@ -0,0 +1,37 @@
+USE escuela;
+DELIMITER //
+
+CREATE FUNCTION calcular_promedio_alumno(id_alumno INT) 
+	RETURNS DECIMAL(5,2)
+	DETERMINISTIC
+	BEGIN
+		DECLARE promedio DECIMAL(5,2);
+		
+		SELECT AVG(nota) INTO promedio
+		FROM inscripciones
+		WHERE id_alumno = id_alumno;
+		
+		RETURN promedio;
+	END//
+DELIMITER ;
+
+SELECT calcular_promedio_alumno(1);
+
+DELIMITER //
+CREATE FUNCTION obtener_fecha_finalizacion_curso(id_alumno INT, id_comision INT) 
+	RETURNS DATE
+	DETERMINISTIC
+	BEGIN
+		DECLARE fecha_finalizacion DATE;
+		
+		SELECT c.fecha_finalizacion INTO fecha_finalizacion
+		FROM inscripciones i
+		JOIN comisiones c ON i.id_comision = c.id_comision
+		WHERE i.id_alumno = id_alumno AND i.id_comision = id_comision;
+		
+		RETURN fecha_finalizacion;
+	END//
+
+DELIMITER ;
+
+SELECT obtener_fecha_finalizacion_curso(1,1);
